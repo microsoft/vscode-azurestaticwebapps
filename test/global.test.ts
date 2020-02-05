a73d0f49bc8dacebc80c5e930cfb4248a78146b0
Eric Jizba <erijiz@microsoft.com>
2020-02-05 14:03:50 -0800

    Add basic test

---
+++ test/global.test.ts
@@ -0,0 +1,28 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { IHookCallbackContext } from 'mocha';
+import * as vscode from 'vscode';
+import { TestOutputChannel, TestUserInput } from 'vscode-azureextensiondev';
+import { ext, IActionContext } from '../extension.bundle';
+
+export let longRunningTestsEnabled: boolean;
+export let testUserInput: TestUserInput = new TestUserInput(vscode);
+
+export function createTestActionContext(): IActionContext {
+    return { telemetry: { properties: {}, measurements: {} }, errorHandling: { issueProperties: {} } };
+}
+
+// Runs before all tests
+suiteSetup(async function (this: IHookCallbackContext): Promise<void> {
+    this.timeout(1 * 60 * 1000);
+
+    await vscode.commands.executeCommand('azureResourceGroups.refresh'); // activate the extension before tests begin
+    ext.outputChannel = new TestOutputChannel();
+    ext.ui = testUserInput;
+
+    // tslint:disable-next-line:strict-boolean-expressions
+    longRunningTestsEnabled = !/^(false|0)?$/i.test(process.env.ENABLE_LONG_RUNNING_TESTS || '');
+});
