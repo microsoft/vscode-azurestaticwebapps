38bbb2c4f8cea75b226ca0ed61f077c512688b45
Eric Jizba <erijiz@microsoft.com>
2020-02-14 10:57:43 -0800

    Provide more options for delete confirmation

--- src/commands/deleteResourceGroup.ts
+++ src/commands/deleteResourceGroup.ts
@@ -4,10 +4,11 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { window } from 'vscode';
-import { IActionContext, UserCancelledError } from 'vscode-azureextensionui';
+import { IActionContext, IAzureQuickPickItem, UserCancelledError } from 'vscode-azureextensionui';
 import { ext } from '../extensionVariables';
 import { ResourceGroupTreeItem } from '../tree/ResourceGroupTreeItem';
 import { localize } from '../utils/localize';
+import { settingUtils } from '../utils/settingUtils';
 
 export async function deleteResourceGroup(context: IActionContext, primaryNode?: ResourceGroupTreeItem, selectedNodes?: ResourceGroupTreeItem[]): Promise<void> {
     if (!selectedNodes) {
@@ -20,16 +21,29 @@ export async function deleteResourceGroup(context: IActionContext, primaryNode?:
         selectedNodes = selectedNodes.filter(n => n instanceof ResourceGroupTreeItem);
     }
 
+    const deleteConfirmation: string | undefined = settingUtils.getWorkspaceSetting('deleteConfirmation');
+    if (deleteConfirmation === 'QuickPick') {
+        const selectGroups: string = localize('selectGroups', 'Select a resource group to confirm deletion.');
+        const picks: IAzureQuickPickItem<ResourceGroupTreeItem>[] = selectedNodes.map(n => { return { label: n.name, data: n }; });
+        selectedNodes = (await ext.ui.showQuickPick(picks, { placeHolder: selectGroups, canPickMany: true, suppressPersistence: true })).map(p => p.data);
+    }
+
     for (const node of selectedNodes) {
-        const enterToDelete: string = localize('enterToDelete', 'Enter "{0}" to delete this resource group and all it\'s resources.', node.name);
-        function validateInput(val: string | undefined): string | undefined {
-            return val === node.name ? undefined : enterToDelete;
+        if (deleteConfirmation === 'WarningDialog') {
+            const areYouSureDelete: string = localize('areYouSureDelete', 'Are you sure you want to delete resource group "{0}" and all it\'s resources?', node.name);
+            await ext.ui.showWarningMessage(areYouSureDelete, { modal: true }, { title: localize('delete', 'Delete') }); // no need to check result - cancel will throw error
+        } else if (deleteConfirmation !== 'QuickPick') {
+            const enterToDelete: string = localize('enterToDelete', 'Enter "{0}" to delete this resource group and all it\'s resources.', node.name);
+            function validateInput(val: string | undefined): string | undefined {
+                return val === node.name ? undefined : enterToDelete;
+            }
+            const result: string = await ext.ui.showInputBox({ prompt: enterToDelete, validateInput });
+            if (result !== node.name) { // Check again just in case `validateInput` didn't prevent the input box from closing
+                context.telemetry.properties.cancelStep = 'mismatchDelete';
+                throw new UserCancelledError();
+            }
         }
-        const result: string = await ext.ui.showInputBox({ prompt: enterToDelete, validateInput });
-        if (result !== node.name) { // Check again just in case `validateInput` didn't prevent the input box from closing
-            context.telemetry.properties.cancelStep = 'mismatchDelete';
-            throw new UserCancelledError();
-        }
+
         // don't wait
         // tslint:disable-next-line: no-floating-promises
         node.deleteTreeItem(context);
