c6c3d92fc5c7569c73420e3be400fe167d1f71b3
Eric Jizba <erijiz@microsoft.com>
2020-05-06 18:52:39 -0700

    Remove some unused code

--- src/constants.ts
+++ src/constants.ts
@@ -5,6 +5,5 @@
 
 import { localize } from "./utils/localize";
 
-export const extensionPrefix: string = 'staticWebApps';
 export const githubApiEndpoint: string = 'https://api.github.com';
 export const noWorkspaceError: string = localize('noWorkspace', 'This action cannot be completed because there is no workspace opened.  Please open a workspace.');
