2c96defccd306d05686ee5b5c3fee6fe1f3d1b77
Eric Jizba <erijiz@microsoft.com>
2020-05-07 11:01:07 -0700

    Update tests to verify extension can activate (#27)

--- test/global.test.ts
+++ test/global.test.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as assert from 'assert';
 import { IHookCallbackContext } from 'mocha';
 import * as vscode from 'vscode';
 import { TestOutputChannel, TestUserInput } from 'vscode-azureextensiondev';
@@ -19,10 +20,23 @@ export function createTestActionContext(): IActionContext {
 suiteSetup(async function (this: IHookCallbackContext): Promise<void> {
     this.timeout(1 * 60 * 1000);
 
-    await vscode.commands.executeCommand('staticWebApps.refresh'); // activate the extension before tests begin
+    const extension = vscode.extensions.getExtension('ms-azuretools.vscode-azurestaticwebapps');
+    if (!extension) {
+        assert.fail('Failed to find extension.');
+    } else {
+        await extension.activate();
+    }
+
     ext.outputChannel = new TestOutputChannel();
     ext.ui = testUserInput;
 
     // tslint:disable-next-line:strict-boolean-expressions
     longRunningTestsEnabled = !/^(false|0)?$/i.test(process.env.ENABLE_LONG_RUNNING_TESTS || '');
+});
+
+suite('suite1', () => {
+    test('test1', () => {
+        // suiteSetup only runs if a suite/test exists, so added a placeholder test here so we can at least verify the extension can activate
+        // once actual tests exist, we can remove this
+    });
 });
