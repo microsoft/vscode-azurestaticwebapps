dcfbf1a377666a57688ac4ffef3c4218dff5c8b7
Nathan <naturins@microsoft.com>
2020-05-13 10:59:40 -0700

    Update IAzureResourceTreeItem.ts

--- src/tree/IAzureResourceTreeItem.ts
+++ src/tree/IAzureResourceTreeItem.ts
@@ -1,9 +1,9 @@
-import { AzureTreeItem } from "vscode-azureextensionui";
-
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+
+import { AzureTreeItem } from "vscode-azureextensionui";
 
 export interface IAzureResourceTreeItem extends AzureTreeItem {
     data: {} | undefined;
