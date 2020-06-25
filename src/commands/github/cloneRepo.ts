f0b4c2451b9bed9bb452b16690587c377cb4d27e
Nathan <naturins@microsoft.com>
2020-06-25 13:55:15 -0700

    Add git clone to static web app (#130)

---
+++ src/commands/github/cloneRepo.ts
@@ -0,0 +1,17 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { commands } from 'vscode';
+import { IActionContext } from 'vscode-azureextensionui';
+import { ext } from '../../extensionVariables';
+import { StaticWebAppTreeItem } from '../../tree/StaticWebAppTreeItem';
+
+export async function cloneRepo(context: IActionContext, node?: StaticWebAppTreeItem): Promise<void> {
+    if (!node) {
+        node = await ext.tree.showTreeItemPicker<StaticWebAppTreeItem>(StaticWebAppTreeItem.contextValue, context);
+    }
+
+    await commands.executeCommand('git.clone', node.data.properties.repositoryUrl);
+}
--- src/commands/github/openGitHubRepo.ts
+++ src/commands/github/openGitHubRepo.ts
@@ -4,9 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { IActionContext } from 'vscode-azureextensionui';
-import { ext } from '../extensionVariables';
-import { EnvironmentTreeItem } from '../tree/EnvironmentTreeItem';
-import { openUrl } from '../utils/openUrl';
+import { ext } from '../../extensionVariables';
+import { EnvironmentTreeItem } from '../../tree/EnvironmentTreeItem';
+import { openUrl } from '../../utils/openUrl';
 
 export async function openGitHubRepo(context: IActionContext, node?: EnvironmentTreeItem): Promise<void> {
     if (!node) {
