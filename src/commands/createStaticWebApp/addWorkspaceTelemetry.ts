bb1595910e93b9c6be82f798112846e74807a32c
nturinski <naturins@microsoft.com>
2020-07-30 16:44:28 -0700

    Remove unused try

--- src/commands/createStaticWebApp/addWorkspaceTelemetry.ts
+++ src/commands/createStaticWebApp/addWorkspaceTelemetry.ts
@@ -8,24 +8,21 @@ import { workspace } from 'vscode';
 import { IStaticWebAppWizardContext } from "./IStaticWebAppWizardContext";
 
 export function addWorkspaceTelemetry(wizardContext: IStaticWebAppWizardContext): void {
-    try {
-        wizardContext.telemetry.properties.isGitInstalled = 'true';
+    wizardContext.telemetry.properties.isGitInstalled = 'true';
+    const localGit: git.SimpleGit = git(wizardContext.fsPath);
 
-        const localGit: git.SimpleGit = git(wizardContext.fsPath);
-
-        // tslint:disable-next-line:no-floating-promises
-        localGit.status().then(() => {
-            wizardContext.telemetry.properties.isGitProject = 'true';
-        }).catch((error) => {
-            // tslint:disable: no-unsafe-any
-            if (error.message.indexOf('spawn git ENOENT') >= 0) {
-                wizardContext.telemetry.properties.isGitInstalled = 'false';
-            } else if (error.message.indexOf('fatal: Not a git repository') >= 0) {
-                wizardContext.telemetry.properties.isGitProject = 'false';
-            }
-        }).finally(() => {
-            wizardContext.telemetry.properties.numberOfWorkspaces = !workspace.workspaceFolders ? String(0) : String(workspace.workspaceFolders.length);
-            wizardContext.telemetry.properties.gotRemote = String(!!wizardContext.repoHtmlUrl);
-        });
-    }
+    // tslint:disable-next-line:no-floating-promises
+    localGit.status().then(() => {
+        wizardContext.telemetry.properties.isGitProject = 'true';
+    }).catch((error) => {
+        // tslint:disable: no-unsafe-any
+        if (error.message.indexOf('spawn git ENOENT') >= 0) {
+            wizardContext.telemetry.properties.isGitInstalled = 'false';
+        } else if (error.message.indexOf('fatal: Not a git repository') >= 0) {
+            wizardContext.telemetry.properties.isGitProject = 'false';
+        }
+    }).finally(() => {
+        wizardContext.telemetry.properties.numberOfWorkspaces = !workspace.workspaceFolders ? String(0) : String(workspace.workspaceFolders.length);
+        wizardContext.telemetry.properties.gotRemote = String(!!wizardContext.repoHtmlUrl);
+    });
 }
