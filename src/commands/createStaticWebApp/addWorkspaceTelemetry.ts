693355679eebb7049da6c139e5a537596af65c43
nturinski <naturins@microsoft.com>
2020-07-30 17:09:23 -0700

    PR feedback

--- src/commands/createStaticWebApp/addWorkspaceTelemetry.ts
+++ src/commands/createStaticWebApp/addWorkspaceTelemetry.ts
@@ -5,24 +5,24 @@
 
 import * as git from 'simple-git/promise';
 import { workspace } from 'vscode';
+import { IParsedError, parseError } from 'vscode-azureextensionui';
 import { IStaticWebAppWizardContext } from "./IStaticWebAppWizardContext";
 
 export function addWorkspaceTelemetry(wizardContext: IStaticWebAppWizardContext): void {
     wizardContext.telemetry.properties.isGitInstalled = 'true';
-    const localGit: git.SimpleGit = git(wizardContext.fsPath);
+    wizardContext.telemetry.properties.numberOfWorkspaces = !workspace.workspaceFolders ? String(0) : String(workspace.workspaceFolders.length);
+    wizardContext.telemetry.properties.gotRemote = String(!!wizardContext.repoHtmlUrl);
 
+    const localGit: git.SimpleGit = git(wizardContext.fsPath);
     // tslint:disable-next-line:no-floating-promises
     localGit.status().then(() => {
         wizardContext.telemetry.properties.isGitProject = 'true';
     }).catch((error) => {
-        // tslint:disable: no-unsafe-any
-        if (error.message.indexOf('spawn git ENOENT') >= 0) {
+        const pError: IParsedError = parseError(error);
+        if (pError.message.includes('spawn git ENOENT')) {
             wizardContext.telemetry.properties.isGitInstalled = 'false';
-        } else if (error.message.indexOf('fatal: Not a git repository') >= 0) {
+        } else if (pError.message.includes('fatal: Not a git repository')) {
             wizardContext.telemetry.properties.isGitProject = 'false';
         }
-    }).finally(() => {
-        wizardContext.telemetry.properties.numberOfWorkspaces = !workspace.workspaceFolders ? String(0) : String(workspace.workspaceFolders.length);
-        wizardContext.telemetry.properties.gotRemote = String(!!wizardContext.repoHtmlUrl);
     });
 }
