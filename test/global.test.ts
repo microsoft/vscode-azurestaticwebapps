f229f296b41df269502295a136e1d6f281a292d4
nturinski <naturins@microsoft.com>
2021-04-15 12:14:57 -0700

    Remove all local testing code

--- test/global.test.ts
+++ test/global.test.ts
@@ -4,25 +4,12 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as assert from 'assert';
-import * as fse from 'fs-extra';
-import * as os from 'os';
-import * as path from 'path';
 import * as vscode from 'vscode';
 import { TestOutputChannel, TestUserInput } from 'vscode-azureextensiondev';
 import { ext, IActionContext } from '../extension.bundle';
-import { getRandomHexString } from './getRandomHexString';
 
 export let longRunningTestsEnabled: boolean;
 export const testUserInput: TestUserInput = new TestUserInput(vscode);
-
-/**
- * Folder for most tests that do not need a workspace open
- */
-export const testFolderPath: string = path.join(os.tmpdir(), `azSwaTest${getRandomHexString()}`);
-/**
- * Folder for tests that require a workspace
- */
-export let testWorkspacePath: string;
 
 export function createTestActionContext(): IActionContext {
     return { telemetry: { properties: {}, measurements: {} }, errorHandling: { issueProperties: {} }, ui: testUserInput, valuesToMask: [] };
@@ -39,9 +26,6 @@ suiteSetup(async function (this: Mocha.Context): Promise<void> {
         await extension.activate();
     }
 
-    await fse.ensureDir(testFolderPath);
-    await fse.emptyDir(testFolderPath);
-    testWorkspacePath = await initTestWorkspacePath();
 
     ext.outputChannel = new TestOutputChannel();
     ext.ui = testUserInput;
@@ -49,21 +33,10 @@ suiteSetup(async function (this: Mocha.Context): Promise<void> {
     longRunningTestsEnabled = !/^(false|0)?$/i.test(process.env.ENABLE_LONG_RUNNING_TESTS || '');
 });
 
-export async function cleanTestWorkspace(): Promise<void> {
-    await fse.emptyDir(testWorkspacePath);
-}
-
-async function initTestWorkspacePath(): Promise<string> {
-    const workspaceFolders: readonly vscode.WorkspaceFolder[] | undefined = vscode.workspace.workspaceFolders;
-    if (!workspaceFolders) {
-        throw new Error("No workspace is open");
-    } else {
-        assert.strictEqual(workspaceFolders.length, 1, "Expected only one workspace to be open.");
-        const workspacePath: string = workspaceFolders[0].uri.fsPath;
-        assert.strictEqual(path.basename(workspacePath), 'testWorkspace', "Opened against an unexpected workspace.");
-        await fse.ensureDir(workspacePath);
-        await fse.emptyDir(workspacePath);
-        return workspacePath;
-    }
-}
+suite('suite1', () => {
+    test('test1', () => {
+        // suiteSetup only runs if a suite/test exists, so added a placeholder test here so we can at least verify the extension can activate
+        // once actual tests exist, we can remove this
+    });
+});
 
