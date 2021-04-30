95d55f490f7ae89dce0d5e9060e87a5c4890d5f7
Will Lorey <wilorey@microsoft.com>
2021-04-30 10:16:10 -0700

    Remove local project feature flag (#367)

--- src/tree/EnvironmentTreeItem.ts
+++ src/tree/EnvironmentTreeItem.ts
@@ -8,7 +8,7 @@ import { ProgressLocation, ThemeIcon, window } from "vscode";
 import { AppSettingsTreeItem, AppSettingTreeItem } from "vscode-azureappservice";
 import { AzExtTreeItem, AzureParentTreeItem, GenericTreeItem, IActionContext, TreeItemIconPath } from "vscode-azureextensionui";
 import { AppSettingsClient } from "../commands/appSettings/AppSettingsClient";
-import { enableLocalProjectView, onlyGitHubSupported, productionEnvironmentName } from "../constants";
+import { onlyGitHubSupported, productionEnvironmentName } from "../constants";
 import { ext } from "../extensionVariables";
 import { createWebSiteClient } from "../utils/azureClients";
 import { pollAzureAsyncOperation } from "../utils/azureUtils";
@@ -17,7 +17,6 @@ import { tryGetLocalBranch } from "../utils/gitUtils";
 import { localize } from "../utils/localize";
 import { nonNullProp } from "../utils/nonNull";
 import { openUrl } from "../utils/openUrl";
-import { getWorkspaceSetting } from "../utils/settingsUtils";
 import { treeUtils } from "../utils/treeUtils";
 import { getSingleRootFsPath } from "../utils/workspaceUtils";
 import { ActionsTreeItem } from "./ActionsTreeItem";
@@ -105,7 +104,7 @@ export class EnvironmentTreeItem extends AzureParentTreeItem implements IAzureRe
 
     public async loadMoreChildrenImpl(_clearCache: boolean, context: IActionContext): Promise<AzExtTreeItem[]> {
         const children: AzExtTreeItem[] = [this.actionsTreeItem];
-        if (getWorkspaceSetting(enableLocalProjectView) && this.inWorkspace) {
+        if (this.inWorkspace) {
             children.push(...this.gitHubConfigGroupTreeItems);
         }
 
@@ -181,8 +180,6 @@ export class EnvironmentTreeItem extends AzureParentTreeItem implements IAzureRe
         const branch: string | undefined = remote ? await tryGetLocalBranch() : undefined;
         this.inWorkspace = this.parent.repositoryUrl === remote && this.branch === branch;
 
-        this.gitHubConfigGroupTreeItems = getWorkspaceSetting(enableLocalProjectView) ?
-            await GitHubConfigGroupTreeItem.createGitHubConfigGroupTreeItems(this) :
-            [];
+        this.gitHubConfigGroupTreeItems = await GitHubConfigGroupTreeItem.createGitHubConfigGroupTreeItems(this);
     }
 }
