e2c4e95e3ce15890f1272f76e9fc471045bc4cf0
nturinski <naturins@microsoft.com>
2021-06-29 11:28:41 -0700

    PR feedback

--- src/commands/createStaticWebApp/addLocationTelemetry.ts
+++ src/commands/createStaticWebApp/addLocationTelemetry.ts
@@ -7,16 +7,19 @@ import { IStaticWebAppWizardContext } from "./IStaticWebAppWizardContext";
 
 export function addLocationTelemetry(wizardContext: IStaticWebAppWizardContext, key: 'appLocation' | 'apiLocation' | 'outputLocation', defaultValue: string, valueFromSetting?: string): void {
     const value: string | undefined = wizardContext[key];
+    wizardContext.telemetry.properties[`${key}HasSetting`] = (!!valueFromSetting).toString();
+    wizardContext.telemetry.properties[`${key}MatchesSetting`] = (defaultValue === valueFromSetting).toString();
+
     let telemValue: string;
     if (value === undefined) {
         // no telemetry to add yet
         return;
     } else if (value.length === 0) {
         telemValue = 'empty';
-    } else if (value === valueFromSetting) {
-        telemValue = 'setting';
     } else if (value === defaultValue) {
         telemValue = 'default';
+    } else if (value === valueFromSetting) {
+        telemValue = 'setting';
     } else {
         telemValue = 'nonDefault';
     }
