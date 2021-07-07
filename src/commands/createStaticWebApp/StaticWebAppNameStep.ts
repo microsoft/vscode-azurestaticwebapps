ea3b683160abf178db1fe616b09c6120bb7ff2d4
Nathan <naturins@microsoft.com>
2021-07-07 13:37:23 -0700

    Set relatedNameTask for all creation (#434)

--- src/commands/createStaticWebApp/StaticWebAppNameStep.ts
+++ src/commands/createStaticWebApp/StaticWebAppNameStep.ts
@@ -27,11 +27,9 @@ export class StaticWebAppNameStep extends AzureNameStep<IStaticWebAppWizardConte
             value: await this.getRelatedName(wizardContext, folderName),
             validateInput: async (value: string | undefined): Promise<string | undefined> => await this.validateStaticWebAppName(wizardContext, value)
         })).trim();
-        wizardContext.valuesToMask.push(wizardContext.newStaticWebAppName);
 
-        if (wizardContext.advancedCreation) {
-            wizardContext.relatedNameTask = this.getRelatedName(wizardContext, wizardContext.newStaticWebAppName);
-        }
+        wizardContext.valuesToMask.push(wizardContext.newStaticWebAppName);
+        wizardContext.relatedNameTask = this.getRelatedName(wizardContext, wizardContext.newStaticWebAppName);
     }
 
     public shouldPrompt(wizardContext: IStaticWebAppWizardContext): boolean {
