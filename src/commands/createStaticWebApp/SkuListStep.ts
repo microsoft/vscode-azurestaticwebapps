3f8c36dbd4b38b0c832c6b47d693f767f306605d
alexweininger <alex.weininger@live.com>
2021-08-05 17:54:59 -0700

    Add details about pricing options

--- src/commands/createStaticWebApp/SkuListStep.ts
+++ src/commands/createStaticWebApp/SkuListStep.ts
@@ -5,6 +5,7 @@
 
 import { WebSiteManagementModels } from '@azure/arm-appservice';
 import { AzureWizardPromptStep, IAzureQuickPickItem } from 'vscode-azureextensionui';
+import { freeSkuDescription, freeSkuDetail, standardSkuDescription, standardSkuDetail } from '../../constants';
 import { localize } from '../../utils/localize';
 import { nonNullProp } from '../../utils/nonNull';
 import { IStaticWebAppWizardContext } from "./IStaticWebAppWizardContext";
@@ -14,11 +15,13 @@ export class SkuListStep extends AzureWizardPromptStep<IStaticWebAppWizardContex
         const skus: IAzureQuickPickItem<WebSiteManagementModels.SkuDescription>[] = SkuListStep.getSkus().map(s => {
             return {
                 label: nonNullProp(s, 'name'),
+                detail: s.detail,
+                description: s.description,
                 data: s
             };
         });
 
-        const placeHolder: string = localize('selectSku', 'Select a sku');
+        const placeHolder: string = localize('selectPricingOption', 'Select pricing option');
         context.sku = (await context.ui.showQuickPick(skus, { placeHolder, suppressPersistence: true })).data;
     }
 
@@ -26,16 +29,21 @@ export class SkuListStep extends AzureWizardPromptStep<IStaticWebAppWizardContex
         return !context.sku;
     }
 
-    public static getSkus(): WebSiteManagementModels.SkuDescription[] {
+    public static getSkus(): (WebSiteManagementModels.SkuDescription & { description: string, detail: string })[] {
         // seems incomplete, but only name/tier are necessary for the SiteEnvelope to pick the right sku
         return [
             {
                 name: 'Free',
                 tier: 'Free',
+                description: freeSkuDescription,
+                detail: freeSkuDetail
             },
             {
                 name: 'Standard',
                 tier: 'Standard',
-            }];
+                description: standardSkuDescription,
+                detail: standardSkuDetail
+            }
+        ];
     }
 }
