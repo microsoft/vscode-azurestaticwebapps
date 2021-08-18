eb4616c8f0014a1aae274adb2a02ee988f15afa3
alexweininger <alex.weininger@live.com>
2021-08-18 16:20:27 -0500

    Ensure output location is changed for Angular

--- src/commands/createStaticWebApp/OutputLocationStep.ts
+++ src/commands/createStaticWebApp/OutputLocationStep.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { AzureWizardPromptStep } from "vscode-azureextensionui";
-import { appArtifactSubpathSetting, outputSubpathSetting } from "../../constants";
+import { angularOutputLocation, appArtifactSubpathSetting, outputSubpathSetting } from "../../constants";
 import { localize } from "../../utils/localize";
 import { getWorkspaceSetting } from "../../utils/settingsUtils";
 import { addLocationTelemetry } from "./addLocationTelemetry";
@@ -17,7 +17,13 @@ export class OutputLocationStep extends AzureWizardPromptStep<IStaticWebAppWizar
 
         context.outputLocation = (await context.ui.showInputBox({
             value: workspaceSetting || getWorkspaceSetting(appArtifactSubpathSetting, context.fsPath) || defaultValue,
-            prompt: localize('publishLocation', "Enter the location of your build output relative to your app's location or leave blank if it has no build. For example, setting a value of 'build' when your app location is set to 'app' will cause the content at 'app/build' to be served.")
+            prompt: localize('publishLocation', "Enter the location of your build output relative to your app's location or leave blank if it has no build. For example, setting a value of 'build' when your app location is set to 'app' will cause the content at 'app/build' to be served."),
+            validateInput: (value: string): string | undefined => {
+                if (value === angularOutputLocation) {
+                    return localize('fillProjectName', 'Fill in the name of your Angular project.')
+                }
+                return undefined;
+            }
         })).trim();
 
         addLocationTelemetry(context, 'outputLocation', defaultValue, workspaceSetting);
