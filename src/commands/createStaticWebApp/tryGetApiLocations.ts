5b003ffa0fa03b2fc5aecfd0ebac4ec996033036
nturinski <naturins@microsoft.com>
2021-09-15 11:11:21 -0700

    Remove unneeded export

--- src/commands/createStaticWebApp/tryGetApiLocations.ts
+++ src/commands/createStaticWebApp/tryGetApiLocations.ts
@@ -52,6 +52,6 @@ export async function promptForApiFolder(context: IActionContext, detectedApiLoc
 }
 
 // Use 'host.json' as an indicator that this is a functions project
-export async function isFunctionProject(folderPath: string): Promise<boolean> {
+async function isFunctionProject(folderPath: string): Promise<boolean> {
     return await AzExtFsExtra.pathExists(path.join(folderPath, hostFileName));
 }
