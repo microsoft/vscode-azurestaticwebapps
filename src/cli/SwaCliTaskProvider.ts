ca5bad8da656898405f25fbcf2a0469e4e62c487
Alex Weininger <alex.weininger@live.com>
2021-11-10 13:03:53 -0500

    Add devserver-timeout flag to SWA CLI tasks
    This is to fix #574

--- src/cli/SwaCliTaskProvider.ts
+++ src/cli/SwaCliTaskProvider.ts
@@ -92,6 +92,9 @@ export class SwaTaskProvider implements TaskProvider {
         };
 
         const args: string[] = [addArg(options, 'appLocation', 'app-location'), addArg(options, 'apiLocation', 'api-location'), addArg(options, 'run', 'run', true)];
+
+        // Increase devserver timeout to 3x default. See https://github.com/microsoft/vscode-azurestaticwebapps/issues/574#issuecomment-965590774
+        args.push('--devserver-timeout=90000');
         const command = `swa start ${options.context} ${args.join(' ')}`;
         const task = new Task(
             {
