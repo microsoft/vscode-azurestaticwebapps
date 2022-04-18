24cda6317ab8cdabdd4018cab2a72fdbc9419c34
Alex Weininger <alex.weininger@live.com>
2022-04-18 12:30:29 -0700

    wip fix tasks

--- src/cli/SwaCliTaskProvider.ts
+++ src/cli/SwaCliTaskProvider.ts
@@ -87,24 +87,26 @@ export class SwaTaskProvider implements TaskProvider {
 
     private createSwaCliTask(workspaceFolder: WorkspaceFolder, label: string, options: Pick<SWACLIOptions, 'context' | 'apiLocation' | 'run' | 'appLocation'>): Task {
 
-        const addArg = <T>(object: T, property: keyof T, name?: string, quote?: boolean): string => {
-            return object[property] ? quote ? `--${name ?? property.toString()}=\"${object[property]}\"` : `--${name ?? property.toString()}=${object[property]}` : '';
+        const addArg = <T extends Record<string, string>>(object: T, property: keyof T, name?: string): string[] => {
+            const args: string[] = [];
+            if (object[property]) {
+                args.push(`--${name ?? property.toString()}`);
+                args.push(object[property] as string);
+            }
+            return args;
         };
 
-        const args: string[] = [addArg(options, 'appLocation', 'app-location'), addArg(options, 'apiLocation', 'api-location'), addArg(options, 'run', 'run', true)];
+        const args: string[] = ['start', ...addArg(options, 'appLocation', 'app-location'), ...addArg(options, 'apiLocation', 'api-location'), ...addArg(options, 'run', 'run')];
 
         // Increase devserver timeout to 3x default. See https://github.com/microsoft/vscode-azurestaticwebapps/issues/574#issuecomment-965590774
         args.push('--devserver-timeout=90000');
-        const command = `swa start ${options.context} ${args.join(' ')}`;
         const task = new Task(
-            {
-                type: shell,
-                command,
-            },
+            { type: shell },
             workspaceFolder,
             label,
             swa,
-            new ShellExecution(command,
+            new ShellExecution('swa',
+                args,
                 {
                     // Prevent react-scrips auto opening browser
                     env: {
@@ -116,8 +118,6 @@ export class SwaTaskProvider implements TaskProvider {
         );
 
         task.isBackground = true;
-        task.detail = command;
-
         return task;
     }
 }
