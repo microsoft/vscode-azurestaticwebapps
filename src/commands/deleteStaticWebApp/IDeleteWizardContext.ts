c72da984dd57e08951ea508972962c3d93a3d4ec
Alex Weininger <alex.weininger@live.com>
2023-06-09 10:38:48 -0700

    Update shared packages to v2 (#836)
    * Update shared packages to v2
    
    * Update dev package
    
    * Use import type

--- src/commands/deleteStaticWebApp/IDeleteWizardContext.ts
+++ src/commands/deleteStaticWebApp/IDeleteWizardContext.ts
@@ -3,9 +3,8 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
-import { ISubscriptionContext } from "@microsoft/vscode-azext-dev";
-import { ExecuteActivityContext, IActionContext } from "@microsoft/vscode-azext-utils";
-import { ResolvedStaticWebAppTreeItem } from "../../tree/StaticWebAppTreeItem";
+import type { ExecuteActivityContext, IActionContext, ISubscriptionContext } from "@microsoft/vscode-azext-utils";
+import type { ResolvedStaticWebAppTreeItem } from "../../tree/StaticWebAppTreeItem";
 
 export interface IDeleteWizardContext extends IActionContext, ExecuteActivityContext {
     node?: ResolvedStaticWebAppTreeItem;
