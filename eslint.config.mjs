d799bbef7057838e9b79099d57fdab90d8f5132d
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-11-11 16:16:39 -0500

    eng: Linting updates (#1004)

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { azExtEslintRecommended } from '@microsoft/vscode-azext-eng/eslint'; // Other configurations exist
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+    globalIgnores([
+        'test/testProject/**',
+    ]),
+    azExtEslintRecommended,
+]);
