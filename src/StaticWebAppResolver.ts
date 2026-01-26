e14ce0e2f0775a11eb2d5e3d50976e02a71a2fa5
Megan Mott <meganmott@microsoft.com>
2026-01-26 09:27:02 -0800

    fix lint errors

--- src/StaticWebAppResolver.ts
+++ src/StaticWebAppResolver.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { StaticSiteARMResource } from "@azure/arm-appservice";
 import { callWithTelemetryAndErrorHandling, IActionContext, ISubscriptionContext, nonNullProp } from "@microsoft/vscode-azext-utils";
 import { AppResource, AppResourceResolver, ResolvedAppResourceBase } from "@microsoft/vscode-azext-utils/hostapi";
--- src/utils/createTreeItemsWithErrorHandling.ts
+++ src/utils/createTreeItemsWithErrorHandling.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { AzExtParentTreeItem, AzExtTreeItem, InvalidTreeItem } from "@microsoft/vscode-azext-utils";
 import { isNullOrUndefined } from "util";
 import { localize } from "./localize";
